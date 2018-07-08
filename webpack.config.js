e160b2f708577b2a4fac295af0ad92b06f2c072b
Beta Faccion <betafcc@gmail.com>
2018-07-08 08:39:43 -0300

    Adds tests

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const camelCase = require('lodash.camelcase');
 
 
 const package = require('./package.json');
+const {NODE_ENV='production'} = process.env;
 
 
 const base = {
   output: {
+    filename: 'index.js',
     library: camelCase(package.name.split('/').slice(-1)[0]),
     libraryTarget: 'umd',
     umdNamedDefine: true,
@@ -32,4 +34,4 @@ const envs = {
 }
 
 
-module.exports = (env, argv) => Object.assign({}, base, envs[argv.mode]);
+module.exports = Object.assign({}, base, envs[NODE_ENV]);
