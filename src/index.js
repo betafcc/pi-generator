39d2ad1e0398256ea78c0e20ab86d7b91dd1ecc8
Beta Faccion <betafcc@gmail.com>
2018-07-08 08:30:25 -0300

    Adds basic utilities

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import bigInt from 'big-integer';
 
 
-console.log(bigInt(10));
+export const initialEnv = Object.freeze({
+  q : bigInt(1),
+  r : bigInt(0),
+  t : bigInt(1),
+  k : bigInt(1),
+  n : bigInt(3),
+  l : bigInt(3),
+});
+
+
+export const stringifyEnv = env =>
+  JSON.stringify(env);
+
+
+export const parseEnv = envString =>
+  Object
+    .entries(JSON.parse(envString))
+    .reduce((acc, [k, v]) =>
+      (acc[k] = bigInt(v), acc)
+    , {});
