16b894dda2f614960443e1d193ca936091824332
Beta Faccion <betafcc@gmail.com>
2018-07-10 06:58:58 -0300

    Adds util module

---
+++ packages/demo/src/lib/util.js
@@ -0,0 +1,30 @@
+export const copyElementTextContent = el => {
+    const sel = selectElementText(el);
+    document.execCommand('copy');
+    sel.removeAllRanges();
+};
+
+const selectElementText = el => {
+    const range = document.createRange();
+    range.selectNodeContents(el);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    return selection;
+};
+
+
+export const formatTimeDuration = ms => {
+  const d = Math.floor(ms / 1000 / 60 / 60 / 24);
+  const h = Math.floor(ms / 1000 / 60 / 60) % 24;
+  const m = Math.floor(ms / 1000 / 60) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+
+  if (d)
+    return `${d}d ${h}h ${s}s`;
+  if (h)
+    return `${h}h ${s}s`;
+
+  return `${s}s`;
+};
