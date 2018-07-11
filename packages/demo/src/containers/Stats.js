f8d257110c4b7bf7eafda97865864675721d29db
Beta Faccion <betafcc@gmail.com>
2018-07-11 05:48:46 -0300

    Adds digits calculated

---
+++ packages/demo/src/containers/Stats.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import './Stats.css'
+
+
+const Stats = ({totalDigits=100, timeElapsed=0, className, ...props}) =>
+  <div
+      className={"Stats" + (className ? ' ' + className : '')}
+      {...props}
+      >
+    <TotalDigits totalDigits={totalDigits} />
+    <TimeElapsed timeElapsed={timeElapsed} />
+  </div>
+
+
+const TotalDigits = ({totalDigits, className, ...props}) =>
+  <div
+      className={"TotalDigits" + (className ? ' ' + className : '')}
+      {...props}
+      >
+    {totalDigits.toLocaleString()} digits calculated
+  </div>
+
+
+const TimeElapsed = ({timeElapsed, className, ...props}) =>
+  <div
+      className={"TimeElapsed" + (className ? ' ' + className : '')}
+      {...props}
+      >
+    {timeElapsed} cpu time
+  </div>
+
+
+export default connect(
+  state => ({totalDigits: state.result.length}),
+  dispatch => ({}),
+)(Stats)
